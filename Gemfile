0cdb4e27402ecab88a0a46a6e11053f48189289e
famaranon <famarano@uc.cl>
2016-05-15 11:47:16 -0300

    instagram controller

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.2.3'
 
+gem 'instagram'
 gem 'pg'
 gem 'rails_12factor'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@
 
   # You can have the root of your site routed with "root"
    root 'home#index'
-
+   post 'instagram/tag/buscar' => 'instagram#searchTag'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
